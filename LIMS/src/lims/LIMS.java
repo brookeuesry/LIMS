82cbfef1112919f2251e7f275a66439c601610db
Josh <joshua@hgsengineeringinc.com>
2019-09-12 08:44:30 -0500

    Finished Initial DB Connections and Test Pulls from DB

--- LIMS/src/lims/LIMS.java
+++ LIMS/src/lims/LIMS.java
@@ -1,5 +1,7 @@
 package lims;
 
+import java.sql.SQLException;
+import java.sql.*;
 /**
  *
  * @author Josh
@@ -9,8 +11,15 @@ public class LIMS {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, Exception {
         // TODO code application logic here
+        
+        DatabaseConnector connector = new DatabaseConnector();
+        
+        Connection connection = connector.makeConection("localhost", "3306", "lims", "admin", "hgsit!");
+        
+        System.out.println(connector.viewUsers(connection));
+        
+        connection.close();
     }
-    
 }
